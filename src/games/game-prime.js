57f127fc8c14658f7872a5f7d61c8adf77284689
lagiska <lagiska@gmail.com>
2024-07-02 01:37:46 +0300

    change brain-prime logic

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -6,15 +6,19 @@ const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const taskAndAnswer = () => {
   const task = parseInt(getRandomNumber());
+
   const isPrime = (task) => {
     if (task <= 1) return false;
-    if (task % 2 === 0 && task > 2) return false;
-    const sqRoot = Math.sqrt(task);
-    for (let i = 3; i <= sqRoot; i += 2) {
-      if (task % i === 0) return false;
+    if (task <= 3) return true;
+    if (task % 2 === 0 || task % 3 === 0) return false;
+    for (let i = 5; i * i <= task; i = i + 6) {
+      if (task % i === 0 || task % (i + 2) === 0) {
+        return false;
+      }
     }
     return true;
   };
+
   let correctAnswer;
   if (isPrime === true) {
     correctAnswer = 'yes';
