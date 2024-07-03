b4b0528c60bd6fa6e82fea0b85b563d78024b0c3
lagiska <lagiska@gmail.com>
2024-07-03 11:25:38 +0300

    add isPrime function as a separate file

--- src/games/game-prime.js
+++ src/games/game-prime.js
@@ -1,23 +1,12 @@
 #!/usr/bin/env node
 import playGame from '../index.js';
 import getRandomNumber from '../getRandomNumber.js';
+import isPrime from '../isPrime.js';
 
 const rule = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const taskAndAnswer = () => {
   const task = getRandomNumber(1);
-  const isPrime = (number) => {
-    if (number < 2) {
-      return false;
-    }
-    for (let i = 2; i <= Math.sqrt(number); i += 1) {
-      if (number % i === 0) {
-        return false;
-      }
-    }
-    return true;
-  };
-
   const correctAnswer = isPrime(task) ? 'yes' : 'no';
   return [task, correctAnswer];
 };
